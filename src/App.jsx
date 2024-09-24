26f1b944fc9366677dffd5e70b1f4026c80b7c6d
Kamesh0222 <kameshwaranm@presidio.com>
2024-09-25 01:05:52 +0530

    Routing implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./components/main";
+import Login from "./components/login";
+import Signup from "./components/signup";
 
 const App = () => {
   return (
-    <div>
-      <Main />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </Router>
   );
 };
 
